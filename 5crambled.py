5489706f239722e3eaef0cb8901b99f4be7a3036
GopeshSingal <113394541+GopeshSingal@users.noreply.github.com>
2024-09-28 11:58:09 -0400

    Made the canvas initialize with the pixels, fixed out-of-bounds issues

--- 5crambled.py
+++ 5crambled.py
@@ -8,14 +8,6 @@ grid_size = 16
 ratio = canvas_size / grid_size 
 color = ft.colors.BLACK
 
-background = cv.Fill(
-                ft.Paint(
-                    gradient=ft.PaintLinearGradient(
-                        (0, 0), (600, 600), colors=[ft.colors.CYAN_50, ft.colors.GREY]
-                    )
-                )
-            )
-
 
 class State:
     x: float
@@ -27,52 +19,56 @@ state = State()
 
 def main(page: ft.Page):
     page.title = "Flet Brush"
+    page.bgcolor = "#495da3"
     # page.window.width = canvas_size + 16
     # page.window.height = canvas_size + 32
     # page.window.resizable = False
 
-    def set_pixel():
-        cp.shapes.remove()
+    def init_canvas():
+        shapes = []
+        for i in range(grid_size):
+            for j in range(grid_size):
+                shapes.append(cv.Rect(j * ratio, i * ratio, ratio, ratio, paint=ft.Paint(color="#ffffff", stroke_width=2)))
+        return shapes
 
     # color picker code
     color_picker = ColorPicker(color="#fff8e7")
+
 
     def change_color(e):
         color_picker.color = new_color.value
         color_picker.update()
 
+    def upload():
+        return
+
     
+    def set_pixel(x, y):
+        if x > -1 and x < 512 and y > -1 and y < 512:
+            x = x // ratio
+            y = y // ratio
+            cp.shapes[(int)(x + y * grid_size)].paint.color = color_picker.color
+
+
     def pan_start(e: ft.DragStartEvent):
-        state.x = (e.local_x // ratio)
-        state.y = (e.local_y // ratio)
+        set_pixel(e.local_x, e.local_y)
 
     def pan_update(e: ft.DragUpdateEvent):
-        cp.shapes.append(
-            cv.Rect(
-                state.x * ratio, state.y * ratio, ratio, ratio, paint=ft.Paint(color=color_picker.color, stroke_width=2),
-            )
-        )
-        
+        set_pixel(e.local_x, e.local_y)
         cp.update()
         state.x = (e.local_x // ratio)
         state.y = (e.local_y // ratio)
     def on_tap(e: ft.TapEvent):
-        state.x = (e.local_x // ratio)
-        state.y = (e.local_y // ratio)
-        cp.shapes.append(
-            cv.Rect(
-                state.x * ratio, state.y * ratio, ratio, ratio, paint=ft.Paint(color=color_picker.color, stroke_width=2),
-            )
-        )
+        set_pixel(e.local_x, e.local_y)
         cp.update()
-    def reset_canvas(e):
-        cp.shapes = [background]
+        
+    def reset_canvas(e, color="#ffffff"):
+        for i in range(grid_size):
+            for j in range(grid_size):
+                cp.shapes[(int)(i + j * grid_size)].paint.color = color
         page.update()
 
-    cp = cv.Canvas(
-        [
-            background,
-        ],
+    cp = cv.Canvas(init_canvas(),
         content=ft.GestureDetector(
             on_pan_start=pan_start,
             on_pan_update=pan_update,
@@ -81,13 +77,6 @@ def main(page: ft.Page):
         expand=False,
     )
     )
-
-    col = ft.Column(
-        [
-            color_picker,
-        ]
-    )
-
     row = ft.Row(
         [
             ft.Container(
@@ -97,13 +86,20 @@ def main(page: ft.Page):
                 height=canvas_size,
                 expand=False,
             ),
-            col
+            color_picker,
         ]
     )
-    page.add(
-        row,
-        ft.ElevatedButton(text="Reset", on_click=reset_canvas)
+
+    button_row = ft.Row(
+        [
+            ft.ElevatedButton(text="Reset", on_click=reset_canvas),
+            ft.ElevatedButton(text="Fill", on_click= lambda x: reset_canvas(x, color=color_picker.color)),
+            ft.ElevatedButton(text="Upload", on_click=upload)
+        ]
     )
 
-
+    page.add(
+        row,
+        button_row
+    )
 ft.app(main)

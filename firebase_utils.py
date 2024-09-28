08753069d9cf4d844b5aea9b938b4e8101c9c328
swright92 <swright@gatech.edu>
2024-09-28 19:10:49 -0400

    Basic login with firebase auth rest api

--- firebase_utils.py
+++ firebase_utils.py
@@ -1,8 +1,17 @@
+import json
+
 import firebase_admin
 from firebase_admin import firestore, credentials
+from firebase import firebase
 
 credentials_path = 'secrets.json'
 cred = credentials.Certificate(credentials_path)
 
 app = firebase_admin.initialize_app(cred)
 cloud_firestore = firestore.client()
+
+secrets = {}
+with open(credentials_path) as f:
+    secrets = json.load(f)
+
+firebase_auth_url_template = 'https://identitytoolkit.googleapis.com/v1/{endpoint}?key=' + secrets['web_api_key']
--- login.py
+++ login.py
@@ -1,25 +1,69 @@
 import flet as ft
-import firebase_admin
-from firebase_admin import credentials, auth
+import requests
+from firebase import firebase
+from firebase_admin import auth
+from firebase_utils import firebase_auth_url_template
 
 
 def login_page(page: ft.Page):
-    username = ft.TextField(label='Username', )
-    password = ft.TextField(label='Password', )
+    email_field = ft.TextField(label='Email', width=300)
+    password_field = ft.TextField(label='Password', width=300, password=True, can_reveal_password=True)
 
-    def login(username, password):
-        pass
+    def firebase_post(endpoint: str, body: dict):
+        url = firebase_auth_url_template.format(endpoint=endpoint)
+        headers = {"Content-Type": "application/json"}
+        return requests.post(url, json=body, headers=headers)
 
+    def login(email, password):
+        login_endpoint = 'accounts:signInWithPassword'
+        request_body = {"email":email, "password": password, "returnSecureToken":True}
+        response = firebase_post(login_endpoint, request_body)
+        body = response.json()
+        print(body)
+        if response.ok:
+            page.go('/home')
+
+    def sign_up(email, password):
+        signup_endpoint = 'accounts:signUp'
+        request_body = {"email":email, "password": password, "returnSecureToken":True}
+        response = firebase_post(signup_endpoint, request_body)
+        body = response.json()
+        print(body)
+        if response.ok:
+            page.go('/home')
+
+    login_button = ft.ElevatedButton(text='Login', on_click=lambda _: login(email_field.value, password_field.value))
+    sign_up_button = ft.ElevatedButton(text='Sign up', on_click=lambda _: sign_up(email_field.value, password_field.value))
     input_row = ft.Row(
         [
-            username,
-            password,
-        ]
+            ft.Column(
+                [
+                    email_field,
+                    password_field,
+                    ft.Row(
+                        [
+                            login_button,
+                            sign_up_button
+                        ],
+                        alignment=ft.MainAxisAlignment.CENTER
+                    )
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                expand=True
+            )
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+        expand=True
     )
-    login_button = ft.ElevatedButton(text='Login', on_click=login)
 
     return [
-        ft.AppBar(title=ft.Text("Login"), bgcolor=ft.colors.SURFACE_VARIANT, actions=[ft.ElevatedButton("Go to canvas", on_click=lambda _: page.go("/home"))]),
-        input_row,
-        login_button
+        ft.AppBar(
+            title=ft.Text("Login"),
+            bgcolor=ft.colors.SURFACE_VARIANT,
+            actions=[
+                ft.ElevatedButton("Go to canvas", on_click=lambda _: page.go("/home"))
+            ]
+        ),
+        input_row
     ]
